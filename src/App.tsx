2bd983dc20590a5c9f84a03c93579469fd368028
matheustkaczyk <matheus111904@hotmail.com>
2022-05-24 11:18:16 -0300

    fix: removendo react.strictmode, estava atraplhando

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,33 @@ import Header from './components/Header'
 import QrCode from 'qrcode';
 
 function App() {
+  const teste = async () => {
+    try {
+      await QrCode.toDataURL('https://www.google.com.br', function (err, url) {
+        if (err) console.log(err);
+  
+        console.log(url);
+      });
+      
+      await QrCode.toCanvas('https://www.google.com.br', function (err, canvas) {
+        if (err) console.log(err);
+  
+        let conteiner = document.getElementById('canvas-wrapper') as HTMLElement;
+        conteiner.appendChild(canvas);
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  teste();
+
   return (
     <div className="App">
       <Header />
+      <div id='canvas-wrapper'>
+
+      </div>
     </div>
   )
 }
