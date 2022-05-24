aa7e842c63ccd191eb20bd2cbbdeae28b3202fcb
matheustkaczyk <matheus111904@hotmail.com>
2022-05-24 12:57:52 -0300

    fix: corrigindo equivoco no input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,9 +2,7 @@ import IInput from "./input.interface";
 
 const Input = ({ text, handleChange, type }: IInput) => {
   return (
-    <input type={type} onChange={(e) => handleChange(e)}>
-      {text}
-    </input>
+    <input type={type} onChange={(e) => handleChange(e)} placeholder={text} />
   );
 }
 
