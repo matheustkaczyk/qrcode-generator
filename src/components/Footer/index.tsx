9af217cd112caee99cf7a02743a5a7553d7e4ef6
matheustkaczyk <matheus111904@hotmail.com>
2022-05-27 10:55:56 -0300

    feat: baixando e renderizando icones de github e linkedin na tela

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,18 @@
+import linkedin from '../../images/linkedin.png';
+import github from '../../images/github.png';
+
 const Footer = () => {
   return (
     <footer>
       <p>Matheus Tkaczyk Ribeiro 27/05/2022</p>
+      <div>
+        <a href="https://www.linkedin.com/in/matheustkaczykribeiro/" target="_blank" rel="noopener noreferrer">]
+          <img src={linkedin} alt="linkedin" />
+        </a>
+        <a href="https://www.github.com/matheustkaczyk" target="_blank" rel="noopener noreferrer">
+          <img src={github} alt="github" />
+        </a>
+      </div>
     </footer>
   )
 }
